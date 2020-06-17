7e712ccae7824116e29333f2ea5df4470fe7c9a6
loricode <angel>
2020-06-17 11:18:38 -0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,78 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function Home({ navigation }){
+  return(
+    <View style={styles.container}>
+     <Text>home</Text>
+     
+     <View  style={{flexDirection:'row' }}>
+       <Button
+        title="perfil"
+        onPress={() =>
+        navigation.navigate('Perfil')} 
+        />
+       <Button
+        title="login"
+        onPress={() =>
+        navigation.navigate('Login', { name: 'Juan' }) }
+        />
+     </View>
+    </View>
+  );
+} 
+
+function Login({navigation}){
+  return(
+    <View style={styles.container}>
+      <Text style={{fontSize:29,color:'green'}}>Login</Text>
+      <TextInput
+        style={{ width:400, height: 50,fontSize:22,margin:10}}
+        placeholder="Usuario"
+        onChangeText={nombre => console.log(nombre)}
+      />
+       <TextInput
+        style={{ width:400, height: 50,fontSize:22,margin:10}}
+        placeholder="Password"
+        onChangeText={password => console.log(password)}
+      />
+       <Button
+        color="green"
+        title="Login"
+        onPress={() =>
+        navigation.navigate('Perfil', { name: '' }) }
+        />
+
+    </View>
+  );
+}
+
+function Perfil(){
+  return(
+    <View style={styles.perfilContainer}>
+      <Text style={{fontSize:29,color:'green'}}>Perfil</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: 'Home' }}
+        />
+        <Stack.Screen name="Perfil" component={Perfil} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -13,6 +80,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  perfilContainer: {
+    flex: 1,
+    backgroundColor: '#C2B3DC',
     alignItems: 'center',
     justifyContent: 'center',
   },
